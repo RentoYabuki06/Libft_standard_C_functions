888c13f81063ee3bb76c8cbd9bbd716252a42e8e
rento yabuki <rento.nky.1203@gmail.com>
2025-04-16 14:15:47 +0900

    integration of fd

--- ft_fprintf.c
+++ ft_fprintf.c
@@ -6,13 +6,13 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 11:59:35 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/04/16 12:19:31 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/04/16 14:14:30 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_fprintf1(const char *fmt, const char *arg)
+void	ft_fprintf1(int fd, const char *fmt, const char *arg)
 {
 	char		buf[BUFFER_SIZE];
 	int			i;
@@ -34,10 +34,10 @@ void	ft_fprintf1(const char *fmt, const char *arg)
 			buf[i++] = *fmt++;
 	}
 	buf[i] = '\0';
-	write(STDERR_FILENO, buf, i);
+	write(fd, buf, i);
 }
 
-void	ft_fprintf2(const char *fmt, const char *arg1, const char *arg2)
+void	ft_fprintf2(int fd, const char *fmt, const char *arg1, const char *arg2)
 {
 	char		buf[BUFFER_SIZE];
 	int			i;
@@ -62,5 +62,5 @@ void	ft_fprintf2(const char *fmt, const char *arg1, const char *arg2)
 			buf[i++] = *fmt++;
 	}
 	buf[i] = '\0';
-	write(STDERR_FILENO, buf, i);
+	write(fd, buf, i);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 17:24:13 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/04/16 12:19:43 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/04/16 14:14:48 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ int		ft_atoi(const char *str);
 long	ft_atol(const char *s);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void	ft_fprintf1(const char *fmt, const char *arg);
-void	ft_fprintf2(const char *fmt, const char *arg1, const char *arg2);
+void	ft_fprintf1(int fd, const char *fmt, const char *arg);
+void	ft_fprintf2(int fd, const char *fmt, const char *arg1, const char *arg2);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
